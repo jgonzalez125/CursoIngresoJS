c0affb6ae7c2c22bf82a7d1f949f59fea41cfe1a
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-05 02:32:29 -0300

    commit n°10
    Ejercicios 10 y 11. Utilizando operaciones con variables para realizar un cálculo dinámico y que el usuario pueda ingresar el porcentaje que desee y obtenga la respuesta

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,42 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var importe;
+	var descuento;
+	var result;
+
+	importe = document.getElementById('importe').value;
+	importe = parseInt(importe);
+
+	descuento = 0.75;
+
 	
+
+	result = importe * descuento;
+
+	document.getElementById('resultado').value=result;
+	
+ /*
+ Ejercicio 11:
+
+	var importe1;
+	var descuento1;
+	var resultado;
+
+	importe1 = prompt('Escribe tu importe', '');
+	descuento1 = prompt('Escribe tu porcentaje', '');
+
+	importe1 = parseInt(importe1);
+	descuento1 = parseInt(descuento1);
+
+	resultado = importe1 - (importe1 * descuento1 / 100);	
+
+
+	console.log(resultado);
+*/
+
 }
+
+
+
+	
