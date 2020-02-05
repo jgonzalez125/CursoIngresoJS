6cddcd2db873b23460582a91ec25bbb52f51c202
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-05 02:08:02 -0300

    commit n°9
    Operación de porcentaje

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,34 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var aumento;
+	var result;
+
+	importe = document.getElementById('sueldo').value; 
+	aumento = 1.1;
+
+	result = importe * aumento;
+
+	document.getElementById('resultado').value=result;
 
 	
 }
+
+/*
+Otra forma de hacerlo seria: 
+
+var importe;
+	var aumento;
+	var result;
+
+	importe = document.getElementById('sueldo').value; 
+	aumento = 10;
+
+	result = (importe / 100) * 10;
+
+
+	document.getElementById('resultado').value=result;
+
+	*/
+
