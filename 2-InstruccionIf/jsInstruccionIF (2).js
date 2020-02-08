56396a52b4fe833122f915cf8c2c26d9d16b2efb
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-08 16:34:13 -0300

    commit if 2
    Mostrar mediante if statement si el usuario es mayor de edad

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
 
+	age = document.getElementById('edad').value;
+	age = parseInt(age);
+
+	if (age >= 18) {
+		return alert('Usted es mayor de edad');
+	}
 
 
 }//FIN DE LA FUNCIÓN
