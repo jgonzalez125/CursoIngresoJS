9da62ec3a9a2069deda15ab1bcdc55fcac65dbd5
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-08 16:33:29 -0300

    commit if 1
    Insertar if statement para mostrar una edad especifica

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
+
+	age = document.getElementById('edad').value;
+	age = parseInt(age);
 
 
+	if (age == 15) {
+		return alert('niña bonita');
+	}
 
 }//FIN DE LA FUNCIÓN
