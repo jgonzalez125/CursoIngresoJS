75315135784774e5f63a3dedf14b078d1b36a315
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-08 16:42:56 -0300

    commit if 4
    Mostrar mediante if statement si el usuario es adolescente (entre 13 y 17 años inclusive)

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
 
+	age= document.getElementById('edad').value;
+	age= parseInt(age);
 
+	if( (age <= 17) && (age >= 13) ){
+		return alert('Usted es adolescente');
+	}
 
+/*otra forma de realizar el if: 
+	if (age <= 17 && age >= 13) {
+		return alert('Usted es adolescente');
+	}
+*/
 }//FIN DE LA FUNCIÓN
