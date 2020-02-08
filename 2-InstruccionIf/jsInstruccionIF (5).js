d2717e3c20fe7b3045cc08557517d2f051286d7b
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-08 17:15:56 -0300

    commit if n5
    Uso del if statement y del operador logico | | para mostrarle al usuario si su edad ingresada indica solamente que NO es adolescente

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
+
+	age= document.getElementById('edad').value;
+	age= parseInt(age);
+
+	if( age > 17 || age < 13 ) {
+		return alert('Usted NO es adolescente');
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
