8e8b95a2f08be1249b77b7f928c4e753186444d4
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-08 18:20:49 -0300

    commit if6
    Uso de if else if else para mostrar si el usuario es mayor de edad, si es adolescente o si es niño

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
 
+	age= document.getElementById('edad').value;
+	age= parseInt(age);
+
+	if (age >= 18) {
+		return alert('Usted es mayor de edad');
+	} else if ( age <= 17 && age >= 13 ) {
+		return alert('Usted es adolescente');
+	} else {
+		return alert('Usted es niño');
+	}
 
 
 
