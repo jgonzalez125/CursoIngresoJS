3f248e901bc45fda15a7c37f93f758691d58b695
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-10 20:30:51 -0300

    if en clase 5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,16 +1,34 @@
 function mostrar()
 {
-//tomo la edad  
-	var age;
+	var edad;
+	//tomo la edad  
+	edad = document.getElementById('edad').value;
+
+	if (edad < 13)
+	{
+		alert('Usted NO es adolescente');
+	}
+	if(edad > 17) 
+	{
+		alert('Usted NO es adolescente');
+	}	
+
+
+
+
+
+
+
+/*	var age;
 
 	age= document.getElementById('edad').value;
 	age= parseInt(age);
 
 	if( age > 17 || age < 13 ) {
-		return alert('Usted NO es adolescente');
+		alert('Usted NO es adolescente');
 	}
 
-
+*/
 
 
 }//FIN DE LA FUNCIÓN
