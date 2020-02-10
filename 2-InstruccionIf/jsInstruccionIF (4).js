e0ae3b4fa72bbbeff7b51b0e04cc359c9458dbd5
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-10 20:29:41 -0300

    if en clase 4
    diferente manera de usar el if

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,18 +1,34 @@
 function mostrar()
 {
-//tomo la edad  
-	var age;
+	var edad;
+	//tomo la edad  
+	edad= document.getElementById('edad').value;
+
+	if(edad > 12 ) 
+	{
+		if(edad < 18) 
+		{
+			alert('Usted es adolescente');
+		}
+	}
+
+
+
+
+
+
+/*	var age;
 
 	age= document.getElementById('edad').value;
 	age= parseInt(age);
 
-	if( (age <= 17) && (age >= 13) ){
-		return alert('Usted es adolescente');
+	if( (age < 18) && (age > 12) ){
+		alert('Usted es adolescente');
 	}
 
 /*otra forma de realizar el if: 
 	if (age <= 17 && age >= 13) {
-		return alert('Usted es adolescente');
+		alert('Usted es adolescente');
 	}
 */
 }//FIN DE LA FUNCIÓN
