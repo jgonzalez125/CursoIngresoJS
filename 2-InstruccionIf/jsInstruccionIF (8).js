748f70ca158ac4d5c6f9732a65bbfb373384bf78
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-09 21:02:59 -0300

    commit if n8
    uso de if else if statement para mostrarle un mensaje al usuario <=> ingresa una edad mayor a 18 años y su estado civil es soltero

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,25 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
+	var estadoCivil;
+	var soltero;
+	var casado;
+	var divorciado;
 
+	age= document.getElementById('edad').value;
+	age= parseInt(age);
+
+	estadoCivil = document.getElementById('estadoCivil').value;
+	casado = document.getElementById('estadoCivil').value= 'Casado';
+	divorciado = document.getElementById('estadoCivil').value='Divorciado';
+	soltero = document.getElementById('estadoCivil').value= 'Soltero';
+
+	if (age >= 18 && estadoCivil == soltero){
+		return alert('Es soltero y no es menor.' );
+	} else if (age > 18 && estadoCivil == casado || divorciado) {
+		return null;
+	}
 	
 
 
