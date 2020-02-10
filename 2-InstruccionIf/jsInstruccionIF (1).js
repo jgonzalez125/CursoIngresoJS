2b058c188f40df830e62f47ad3b1f71189a8f0c3
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-10 20:15:21 -0300

    commit if en clase n1
    diferentes formas de realizar un if

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,14 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-	var age;
+	var edad;
+	//tomo la edad  
+	edad = document.getElementById('edad').value;
+
+	if(edad == "15")
+	{
+		alert('niña bonita');
+	} 
+
+
+/*	var age
 
 	age = document.getElementById('edad').value;
 	age = parseInt(age);
 
 
 	if (age == 15) {
-		return alert('niña bonita');
+		alert('niña bonita');
 	}
+*/
+
+
 
 }//FIN DE LA FUNCIÓN
