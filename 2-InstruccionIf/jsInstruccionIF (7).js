559b1b36c5fad4fa3cc1a25ee49541b0b2aab0c0
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-09 21:01:51 -0300

    commit if n7
    uso del if else if statement para decirle al usuario que si ingresa una edad menor a 18 años y tiene otro estado civil distinto a soltero que sus datos no son validos

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,25 @@
 function mostrar()
 {
 //tomo la edad  
+	var age;
+	var estadoCivil;
+	var soltero;
+	var casado;
+	var divorciado;
 
-	
+	age= document.getElementById('edad').value;
+	age= parseInt(age);
 
+	estadoCivil = document.getElementById('estadoCivil').value;
+	casado = document.getElementById('estadoCivil').value= 'Casado';
+	divorciado = document.getElementById('estadoCivil').value='Divorciado';
+	soltero = document.getElementById('estadoCivil').value= 'Soltero';
+
+
+	if (age < 18 && estadoCivil == soltero){
+		return console.log('bien');
+	} else if (age < 18 && estadoCivil == divorciado || casado) {
+		return alert('Es muy pequeño para NO ser soltero.');
+	}
 
 }//FIN DE LA FUNCIÓN
