428ce8bdac519140c90470d69126d8cbe5bc7c4c
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-10 20:18:02 -0300

    if en clase 2
    diferentes formas de realizar el if

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
-//tomo la edad  
-	var age;
+	var edad;
+	//tomo la edad  
+	edad = document.getElementById('edad').value;
+
+	if(edad > 17)
+	{
+		alert('Usted es mayor de edad');
+	}
+
+
+
+
+
+
+
+
+
+	/*var age;
 
 	age = document.getElementById('edad').value;
 	age = parseInt(age);
@@ -9,6 +25,6 @@ function mostrar()
 	if (age >= 18) {
 		return alert('Usted es mayor de edad');
 	}
-
+*/
 
 }//FIN DE LA FUNCIÓN
