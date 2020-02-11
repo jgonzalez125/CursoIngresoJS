669000bd8bc5d9cbf88525f4e124a873099ff4ff
Juan <juan.g.conti@gmail.com>
2020-02-11 19:49:06 -0300

    mi segundo commit

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,7 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
+{//segundo commit
 	var nombre;
 	nombre = prompt('Escribi tu nombre','');
 	alert(nombre);
