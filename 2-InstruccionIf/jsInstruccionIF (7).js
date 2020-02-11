fbea5eedb65752cf39c0b86d70a115cf7f99a4b8
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-10 21:11:04 -0300

    if en clase 7
    forma mas simplificada de como hacer el if

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -4,16 +4,16 @@ function mostrar()
 	var age;
 	var estadoCivil;
 	var soltero;
-	var casado;
-	var divorciado;
+//	var casado;
+//	var divorciado;
 
 	age= document.getElementById('edad').value;
 	age= parseInt(age);
 
 	estadoCivil = document.getElementById('estadoCivil').value;
-/*	casado = document.getElementById('estadoCivil').value= 'Casado';
-	divorciado = document.getElementById('estadoCivil').value='Divorciado';
-*/	soltero = document.getElementById('estadoCivil').value= 'Soltero';
+//	casado = document.getElementById('estadoCivil').value= 'Casado';
+//	divorciado = document.getElementById('estadoCivil').value='Divorciado';
+	soltero = 'Soltero';
 
 		if (age < 18 && estadoCivil != soltero){
 		 alert('Es muy pequeño para NO ser soltero.');
