9be9a562a3ea44863aca88f7f0be538db5a82deb
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-11 00:03:49 -0300

    if commit 10
    Numero random que genera nota de examen con mensajes dependiendo de la nota

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,37 @@
 function mostrar()
 {
+	var num;
 	//Genero el número RANDOM entre 1 y 10 
-	
+	num = Math.round((Math.random() * 10 + 1));
+
+	if(num > 8)
+	{
+		alert(num +" EXCELENTE");
+	} else 
+	{
+		if(num < 4)
+		{
+			alert(num +" Vamos, la proxima se puede");
+		}
+		 	else{
+				alert( num +" APROBÓ");
+		}
+	}
+
+/*	if(num < 8 && num > 3)
+	{
+		alert(num+' aprobó');
+	} else 
+	{
+		if(num > 8) 
+		{
+			alert(num +' EXCELENTE');
+		} else
+		{
+			alert(num + ' vamos, la proxima se puede');
+		}
+	}
+*/
+
 
 }//FIN DE LA FUNCIÓN
