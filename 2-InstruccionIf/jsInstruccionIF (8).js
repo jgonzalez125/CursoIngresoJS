238c4976e81ca470e8a556e6f7fda6804def87a2
Juan <juan.g.conti@gmail.com>
2020-02-11 20:06:11 -0300

    commit if en limpio

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -3,30 +3,18 @@ function mostrar()
 //tomo la edad  
 	var age;
 	var estadoCivil;
-//	var soltero;
-//	var casado;
-//	var divorciado;
 
 	age= document.getElementById('edad').value;
 	age= parseInt(age);
 
-
 	estadoCivil = document.getElementById('estadoCivil').value;
-//	casado = document.getElementById('estadoCivil').value= 'Casado';
-//	divorciado = document.getElementById('estadoCivil').value='Divorciado';
-//	soltero = document.getElementById('estadoCivil').value= 'Soltero';
-	
+
 	if (age > 17 && estadoCivil == 'Soltero')
 	{
 		 alert('Es soltero y no es menor.');
 	} 
 
-/*	if (age > 17 && estadoCivil == soltero){
-		 alert('Es soltero y no es menor.' );
-	} else if (age > 18 && estadoCivil == casado || divorciado) {
-		 null;
-	}
-*/	
+
 
 
 }//FIN DE LA FUNCIÓN
