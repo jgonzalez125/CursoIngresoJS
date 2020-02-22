162b6e3d3e8f44c91d6d2126304eedadc936da61
jgonzalez125 <60628744+jgonzalez125@users.noreply.github.com>
2020-02-22 10:55:23 -0300

    Commit while mejorado

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,11 +2,24 @@ function mostrar()
 {
 
 	var sexo = prompt("ingrese f ó m .");
+	var femenino= 'Femenino';
+	var masculino= 'Masculino';
 
 	while(sexo != 'f' && sexo != 'm')
 	{
 		sexo = prompt("reingrese f ó m .");
+		break;
 	}
 	document.getElementById('Sexo').value= sexo;
+
+	switch(sexo)
+	{
+		case 'f':
+		document.getElementById('Sexo').value=femenino;
+		break;
+		case 'm':
+		document.getElementById('Sexo').value=masculino;
+		break;
+	}
 
 }//FIN DE LA FUNCIÓN
